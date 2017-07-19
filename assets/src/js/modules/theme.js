1a7fdc246a5d925e95289d46623b17d17b363d8a
Andrew McNaughton <andrew@andymc.com.au>
2017-07-19 10:44:10 +0200

    project init

---
+++ assets/src/js/modules/theme.js
@@ -0,0 +1,28 @@
+// ------------------------------------
+//
+// Theme
+//
+// ------------------------------------
+
+(function($) {
+
+	if (typeof window.Theme == 'undefined') window.Theme = {};
+
+	Theme = {
+
+		settings: {},
+
+		/*
+		 * Theme init
+		 */
+
+		init: function() {
+
+			// Init 
+
+		}
+	};
+	
+	module.exports = Theme;
+
+})(jQuery);
